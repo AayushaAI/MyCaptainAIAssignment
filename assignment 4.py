333885822861d4557f5ffba9f4de68cb16939c26
AayushaAI <aayusha2120@gmail.com>
2024-10-26 23:14:31 +0530

    Add files via upload
    This python program will print all positive numbers in a range where we input all the positive and negative numbers and the output will only print the positive numbers among them once executing

---
+++ assignment 4.py
@@ -0,0 +1,7 @@
+Python 3.12.7 (tags/v3.12.7:0b05ead, Oct  1 2024, 03:06:41) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:/Users/aayus/Desktop/assignment 4.py ===============
+Enter numbers separated by spaces: 12 -7 5 64 -14
+Positive numbers are:
+12 5 64 
